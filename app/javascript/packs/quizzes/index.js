82b77f2f3ba166e8424e03f4618320b5ae6f0ac9
pipopotamasu <ec0156hx@gmail.com>
2018-07-15 10:17:29 +0900

    vuetify setting

--- app/javascript/packs/quizzes/index.js
+++ app/javascript/packs/quizzes/index.js
@@ -1,6 +1,14 @@
 import Vue from "vue";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+
+Vue.use(Vuetify);
 
 new Vue({
   el: "#quiz-master",
-  template: "<div>fuga!</div>"
+  template: `<v-app>
+              <v-content>
+                <v-container>Hello world</v-container>
+              </v-content>
+            </v-app>`
 });
