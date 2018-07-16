467a3734b9dbc56090fde67e078474207b77bbda
pipopotamasu <ec0156hx@gmail.com>
2018-07-16 16:26:08 +0900

    axios setting

--- app/javascript/packs/quizzes/index.js
+++ app/javascript/packs/quizzes/index.js
@@ -1,7 +1,10 @@
 import Vue from "vue";
 import Vuetify from "vuetify";
+import axios from "axios";
+import VueAxiosPlugin from "@/plugins/vue_axios";
 import QuizMaster from "@/components/quizzes/QuizMaster.vue";
 
+Vue.use(VueAxiosPlugin, { axios: axios });
 Vue.use(Vuetify);
 
 new Vue({
