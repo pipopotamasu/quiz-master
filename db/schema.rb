7b6370a2886d095b95ceadc3d6a08d226a68120d
pipopotamasu <ec0156hx@gmail.com>
2018-07-22 20:52:37 +0900

    use postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180712151053) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "quizzes", force: :cascade do |t|
     t.text "content"
     t.string "answer"
